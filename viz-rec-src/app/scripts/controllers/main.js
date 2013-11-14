f0d9a851b10cf7730e373fab5f535976993f19d2
Kanit Wongsuphasawat <kanitw@gmail.com>
2013-11-13 23:11:02 -0800

    hide bin col; refactor dataManager to remove redundancy; prevent clicking self, which is useless.

--- viz-rec-src/app/scripts/controllers/main.js
+++ viz-rec-src/app/scripts/controllers/main.js
@@ -2,12 +2,12 @@
 
 angular.module('vizRecSrcApp')
   .controller('MainCtrl', function ($scope, dataManager) {
-    dataManager.load("data/movies.json", "movies", function callback(dataTable){
+    dataManager.load("data/movies.json", "movies", true, function callback(dataTable){
       console.log("data loaded!");
       $scope.dataTable = dataTable;
-      //TODO(kanitw): remove this
       $scope.fix = {type:'nominal', values:['1','1','2']};
       $scope.select = function(col){
+        if($scope.selectedField==col)return; //Do nothing
         $scope.selectedField = col;
         $scope.colPairs = _(dataTable).filter(function(c){return c!=col;})
           .map(function(c){ return [col,c]; }).value();
--- viz-rec-src/app/scripts/services/dataManager.js
+++ viz-rec-src/app/scripts/services/dataManager.js
@@ -14,40 +14,39 @@ angular.module('vizRecSrcApp')
       return _.map(counter, function(c,v){ return {val:v, count:c}; });  //{keys:_.keys(counter), count: _.values(counter)};
     }
 
-    var dataManager = {
+    return {
       /** data arrays */
       _: [],
       currentData: null,
-      load: function(path,key, saveData, callback){
-        var self=this;
+      load: function (path, key, saveData, callback) {
+        var self = this;
         var bin20 = dv.bin(20);
 
         key = key || _.last(path.split("/")).split(".")[0];
-        $http.get(path).success(function loadData(data){
+        $http.get(path).success(function loadData(data) {
           data = self._[key] = dv.table(data);
-          if(saveData) self.currentData = data;
+          if (saveData) self.currentData = data;
 
           var originalDataLength = data.length;
 
-          for(var i=0; i<originalDataLength; i++){
+          for (var i = 0; i < originalDataLength; i++) {
             data[i].countTable = countTable(data[i]);
 
-            if (data[i].type == dv.type.numeric){
+            if (data[i].type == dv.type.numeric) {
               var binned = dv.bin(20).array(data[i]);
-              data[i].bin20 = data.addColumn(data[i].name+":bin20", binned, dv.type.ordinal);
+              data[i].bin20 = data.addColumn(data[i].name + ":bin20", binned, dv.type.ordinal);
               data[i].bin20.countTable = countTable(data[i].bin20);
+              data[i].bin20.isBinCol = true;
             }
           }
-          if(callback) callback(data);
+          if (callback) callback(data);
         });
       },
-      get: function(key){
+      get: function (key) {
         return key ? _[key] : this.currentData;
       },
-      remove: function(key){
+      remove: function (key) {
         delete _[key];
       }
-    }
-
-    return dataManager;
+    };
   });
