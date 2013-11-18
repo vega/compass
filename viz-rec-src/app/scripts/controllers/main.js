39413b5240c55c36fe15efd84c4a21ad6a554878
Kanit Wongsuphasawat <kanitw@gmail.com>
2013-11-18 10:26:49 -0800

    2d now sort by cardinality

--- viz-rec-src/app/scripts/controllers/main.js
+++ viz-rec-src/app/scripts/controllers/main.js
@@ -10,7 +10,12 @@ angular.module('vizRecSrcApp')
         if($scope.selectedField==col)return; //Do nothing
         $scope.selectedField = col;
         $scope.colPairs = _(dataTable).filter(function(c){return c!=col;})
-          .map(function(c){ return [col,c]; }).value();
+          .map(function(c){ return [col,c]; })
+          .sortBy(function(p){
+            // CARDINALITY
+            return (p[1].type == dv.type.numeric ? 20 : p[1].countTable.length);
+          })
+          .value();
       }
 
       $scope.select(dataTable[0]);
