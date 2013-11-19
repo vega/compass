9168b188d7cf99810190ad71c8caf3f8a87559d4
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2013-11-18 21:03:46 -0800

    Simple Date support #11

--- viz-rec-src/app/scripts/services/dataManager.js
+++ viz-rec-src/app/scripts/services/dataManager.js
@@ -20,7 +20,8 @@ angular.module('vizRecSrcApp')
       currentData: null,
       load: function (path, key, saveData, callback) {
         var self = this;
-        var bin20 = dv.bin(20);
+        var BIN_LEVEL = 20;
+        var bin20 = dv.bin(BIN_LEVEL);
 
         key = key || _.last(path.split("/")).split(".")[0];
         $http.get(path).success(function loadData(data) {
@@ -28,15 +29,27 @@ angular.module('vizRecSrcApp')
           if (saveData) self.currentData = data;
 
           var originalDataLength = data.length;
-
-          for (var i = 0; i < originalDataLength; i++) {
+          var i,j;
+          for (i = 0; i < originalDataLength; i++) {
             data[i].countTable = countTable(data[i]);
 
             if (data[i].type == dv.type.numeric) {
               var binned = dv.bin(20).array(data[i]);
               data[i].bin20 = data.addColumn(data[i].name + ":bin20", binned, dv.type.ordinal, null, true);
               data[i].bin20.countTable = countTable(data[i].bin20);
-              data[i].bin20.isBinCol = true;
+              data[i].bin20.binLevel = 20;
+            }else if(data[i].type == dv.type.date){
+              var dateData = data[i].map(function(v){ return new Date(v); });
+              var dateBinner = {
+                year: function(d){ return d.getFullYear();},
+                month: function(d){ return d.getMonth();},
+                day: function(d){ return d.getDay();}
+              };
+              _.each(dateBinner, function(binner, level){
+                data[i][level] = data.addColumn(data[i].name+":"+level, dateData.map(binner), dv.type.ordinal);
+                data[i][level].countTable = countTable(data[i][level]);
+                data[i][level].binLevel = level;
+              });
             }
           }
           if (callback) callback(data);
