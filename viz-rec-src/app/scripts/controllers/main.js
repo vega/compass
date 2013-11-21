5b597196a88b93538673dd9ec9c01c2d4fa9d0d2
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2013-11-20 23:33:41 -0800

    enable select to swap rankings

--- viz-rec-src/app/scripts/controllers/main.js
+++ viz-rec-src/app/scripts/controllers/main.js
@@ -2,19 +2,46 @@
 
 angular.module('vizRecSrcApp')
   .controller('MainCtrl', function ($scope, dataManager) {
-    $scope.sorter = {
-      cardinality: function(pair){
-        return (pair[1].type == dv.type.numeric ? 20 : pair[1].countTable.length);
-      },
-      mutualInformationDistance: function(pair){
-        return dataManager.currentData.mi_distance[pair[0].index][pair[1].index];
-      }
+    function setSorter(sorterType){
+      $scope.currentSorter = $scope.sorter2d[sorterType];
+      updatePairs();
+
+    }
+    function updatePairs(){
+      var col = $scope.selectedField, dataTable = $scope.dataTable, currentSorter = $scope.currentSorter;
+      var _colPairs = _(dataTable).filter(function(c,i){return c!=col && i < dataTable.originalLength;})
+        .map(function(c){
+          var pair = [col,c];
+          pair.metric = currentSorter.metric(pair);
+          return pair;
+        })
+        .sortBy("metric");
+
+      $scope.colPairs = currentSorter.reverse ? _colPairs.reverse().value(): _colPairs.value();
     }
 
-    $scope.currentSorter = $scope.sorter.mutualInformationDistance;
-    $scope.setSorter = function(sorterType){
-      $scope.currentSorter = $scope.sorter[sorterType];
+    /** map of type of sorter */
+    $scope.sorter2d = {
+      cardinality: {
+        metric: function(pair){
+          return (pair[1].type == dv.type.numeric ? 20 : pair[1].countTable.length);
+        },
+        reverse: false
+      },
+      mutualInformationDistance: {
+        metric: function(pair){
+          return dataManager.currentData.mi_distance[pair[0].index][pair[1].index];
+        },
+        reverse: true
+      }
     };
+
+
+    $scope.sorter2dTypes = _.keys($scope.sorter2d);
+    $scope.currentSorterType = "mutualInformationDistance";
+
+    $scope.$watch("currentSorterType", setSorter);
+
 
     dataManager.load("data/movies.json", "movies", true, function callback(dataTable){
       console.log("data loaded!");
@@ -23,15 +50,7 @@ angular.module('vizRecSrcApp')
       $scope.select = function(col){
         if($scope.selectedField==col)return; //Do nothing
         $scope.selectedField = col;
-        $scope.colPairs = _(dataTable).filter(function(c,i){return c!=col && i < dataTable.originalLength;})
-          .map(function(c){
-            var pair = [col,c];
-            pair.metric = $scope.currentSorter(pair);
-            return pair;
-          })
-          .sortBy("metric")
-          .reverse()
-          .value();
+        updatePairs();
       };
 
       $scope.select(dataTable[0]);
