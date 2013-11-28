9c600d3c6254bb55f4bba91e35d31157e85fb47e
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2013-11-28 13:04:26 -0800

    Filter Zero bug (function name mismatch)

--- viz-rec-src/app/scripts/directives/chart1d.js
+++ viz-rec-src/app/scripts/directives/chart1d.js
@@ -210,7 +210,7 @@ angular.module('vizRecSrcApp')
 //          _updateChart();
         };
 
-        scope.toggleIsZeroNull = function(){
+        scope.toggleFilterZero = function(){
           scope.col.filterZero = !scope.col.filterZero;
           scope.col.setFilter(scope.col.filterZero ? helper.isNonZero : null);
         };
