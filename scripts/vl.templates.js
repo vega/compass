vega/compass

commit 34c2ad0dc8e14587a46d1603776b75c3db74413c

quick fix

scripts/vl.templates.js
@@ -100,7 +100,7 @@
 
 
   vlTemplates.marks_bar = function(opt){
-    var color = opt.color || {"value": "__markscolor__"};
+    var color = (opt||{}.color) || {"value": "__markscolor__"};
 
     return {
       "type": "rect",

commit fd38adb43cdb8b182ac5574f2de5681e2fb0b10b

Fix bugs introduced in 34c2ad0dc8e14587a46d1603776b75c3db74413c

scripts/vl.templates.js
@@ -107,7 +107,7 @@
 
 
   vlTemplates.marks_bar = function(opt){
-    var color = (opt||{}.color) || {"value": "__markscolor__"};
+    var color = ((opt||{}).color) || {"value": "__markscolor__"};
 
     return {
       "type": "rect",