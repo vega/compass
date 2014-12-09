8efcd6dd3518cf102dbdf64eb3709ecb875e28eb
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2014-12-08 19:42:48 -0800

    update distance preset

--- src/visgen.js
+++ src/visgen.js
@@ -64,10 +64,15 @@
   // Begin of Distance
 
   var DIST_BY_ENCTYPE = [
+    // positional
     ["x", "y", 0.2],
-    ["color", "shape", 0.2],
     ["row", "col", 0.2],
-    ["color", "alpha", 0.2]
+    // ordinal LOD
+    ["color", "shape", 0.2],
+    // quantitative LOD
+    ["color", "alpha", 0.2],
+    ["size", "alpha", 0.2],
+    ["size", "color", 0.2]
   ].reduce(function(r, x){
     var a=x[0], b=x[1], d=x[2];
     r[a] = r[a] || {};
