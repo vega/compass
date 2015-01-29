b19c436718fb68cb58db2da66e833ae54d2019a2
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-29 06:37:20 -0800

    add powerset code to util

--- src/util.js
+++ src/util.js
@@ -77,3 +77,66 @@ util.gen.getOpt = function (opt) {
   }, Object.create(consts.gen.DEFAULT_OPT));
 };
 
+/**
+ * powerset code from http://rosettacode.org/wiki/Power_Set#JavaScript
+ *
+ *   var res = powerset([1,2,3,4]);
+ *
+ * returns
+ *
+ * [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3],[4],[1,4],
+ * [2,4],[1,2,4],[3,4],[1,3,4],[2,3,4],[1,2,3,4]]
+[edit]
+*/
+
+util.powerset = function(list) {
+  var ps = [
+    []
+  ];
+  for (var i = 0; i < list.length; i++) {
+    for (var j = 0, len = ps.length; j < len; j++) {
+      ps.push(ps[j].concat(list[i]));
+    }
+  }
+  return ps;
+};
+
+util.chooseKorLess = function(list, k) {
+  var subset = [[]];
+  for (var i = 0; i < list.length; i++) {
+    for (var j = 0, len = subset.length; j < len; j++) {
+      var sub = subset[j].concat(list[i]);
+      if(sub.length <= k){
+        subset.push(sub);
+      }
+    }
+  }
+  return subset;
+};
+
+util.chooseK = function(list, k) {
+  var subset = [[]];
+  var kArray =[];
+  for (var i = 0; i < list.length; i++) {
+    for (var j = 0, len = subset.length; j < len; j++) {
+      var sub = subset[j].concat(list[i]);
+      if(sub.length < k){
+        subset.push(sub);
+      }else if (sub.length === k){
+        kArray.push(sub);
+      }
+    }
+  }
+  return kArray;
+};
+
+util.cross = function(a,b){
+  var x = [];
+  for(var i=0; i< a.length; i++){
+    for(var j=0;j< b.length; j++){
+      x.push(a[i].concat(b[j]));
+    }
+  }
+  return x;
+};
+
