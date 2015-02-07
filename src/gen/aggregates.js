07f8aadc836131d0bc0c6798aca9da742a0367f7
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-02-06 23:22:44 -0800

    fixes #25 add time support
    - refactor  vr.gen.aggregates
    - add test case

--- src/gen/aggregates.js
+++ src/gen/aggregates.js
@@ -2,86 +2,122 @@
 
 var vl = require('vegalite');
 
-var util = require('../util');
+var util = require('../util'),
+  consts = require('../consts');
 
 module.exports = genAggregates;
 
 function genAggregates(output, fields, opt) {
+  opt = vl.schema.util.extend(opt||{}, consts.gen.aggregates);
   var tf = new Array(fields.length);
-  opt = util.gen.getOpt(opt);
-  // console.log('vr.gen.aggregates', fields, opt);
 
-  function assignField(i, hasAggr) {
-    // If all fields are assigned, save
-    if (i === fields.length) {
-      if (opt.omitAggregateWithMeasureOnly || opt.omitDimensionOnly) {
-        var hasMeasure = false, hasDimension = false, hasRaw = false;
-        tf.forEach(function(f) {
-          if (util.isDim(f)) {
-            hasDimension = true;
-          } else {
-            hasMeasure = true;
-            if (!f.aggr) hasRaw = true;
+  function checkAndPush() {
+    if (opt.omitMeasureOnly || opt.omitDimensionOnly) {
+      var hasMeasure = false, hasDimension = false, hasRaw = false;
+      tf.forEach(function(f) {
+        if (util.isDim(f)) {
+          hasDimension = true;
+        } else {
+          hasMeasure = true;
+          if (!f.aggr) hasRaw = true;
+        }
+      });
+      if (!hasMeasure && opt.omitDimensionOnly) return;
+      if (!hasDimension && !hasRaw && opt.omitMeasureOnly) return;
+    }
+
+    var fieldSet = vl.duplicate(tf);
+    fieldSet.key = vl.field.shorthands(fieldSet);
+
+    output.push(fieldSet);
+  }
+
+  function assignQ(i, hasAggr) {
+    var f = fields[i],
+      canHaveAggr = hasAggr === true || hasAggr === null,
+      cantHaveAggr = hasAggr === false || hasAggr === null;
+
+    tf[i] = {name: f.name, type: f.type};
+
+    if (f.aggr) {
+      if (canHaveAggr) {
+        tf[i].aggr = f.aggr;
+        assignField(i + 1, true);
+      }
+    } else {
+      var aggregates = (!f._aggr || f._aggr === '*') ? opt.aggrList : f._aggr;
+
+      for (var j in aggregates) {
+        var a = aggregates[j];
+        if (a !== undefined) {
+          if (canHaveAggr) {
+            tf[i].aggr = a;
+            assignField(i + 1, true);
           }
-        });
-        if (!hasMeasure && opt.omitDimensionOnly) return;
-        if (!hasDimension && !hasRaw && opt.omitAggregateWithMeasureOnly) return;
+        } else { // if(a === undefined)
+          if (cantHaveAggr) {
+            delete tf[i].aggr;
+            assignField(i + 1, false);
+          }
+        }
       }
 
-      var fieldSet = vl.duplicate(tf);
-      fieldSet.key = vl.field.shorthands(fieldSet);
+      if (opt.genBin) {
+        // bin the field instead!
+        delete tf[i].aggr;
+        tf[i].bin = true;
+        tf[i].type = 'Q';
+        assignField(i + 1, hasAggr);
+      }
 
-      output.push(fieldSet);
+      if (opt.genTypeCasting) {
+        // we can also change it to dimension (cast type="O")
+        delete tf[i].aggr;
+        delete tf[i].bin;
+        tf[i].type = 'O';
+        assignField(i + 1, hasAggr);
+      }
+    }
+  }
+
+  function assignT(i, hasAggr) {
+    var f = fields[i];
+    tf[i] = {name: f.name, type: f.type};
+
+    if (f.fn) {
+      // FIXME deal with assigned function
+    } else {
+      var fns = (!f._fn || f._fn === '*') ? opt.timeFnList : f._fn;
+      for (var j in fns) {
+        var fn = fns[j];
+        if (fn === undefined) {
+          assignField(i+1, hasAggr);
+        } else {
+          tf[i].fn = fn;
+          assignField(i+1, hasAggr);
+        }
+      }
+    }
+    // FIXME what if you aggregate time?
+
+  }
+
+  function assignField(i, hasAggr) {
+    if (i === fields.length) { // If all fields are assigned
+      checkAndPush();
       return;
     }
 
     var f = fields[i];
-
     // Otherwise, assign i-th field
     switch (f.type) {
       //TODO "D", "G"
       case 'Q':
-        tf[i] = {name: f.name, type: f.type};
-        if (f.aggr) {
-          tf[i].aggr = f.aggr;
-          assignField(i + 1, true);
-        } else {
-          var aggregates = (!f._aggr || f._aggr === '*') ? opt.aggrList : f._aggr;
+        assignQ(i, hasAggr);
+        break;
 
-          for (var j in aggregates) {
-            var a = aggregates[j];
-            if (a !== undefined) {
-              if (hasAggr === true || hasAggr === null) {
-                // must be aggregated, or no constraint
-                //set aggregate to that one
-                tf[i].aggr = a;
-                assignField(i + 1, true);
-              }
-            } else { // if(a === undefined)
-              if (hasAggr === false || hasAggr === null) {
-                // must be raw plot, or no constraint
-                delete tf[i].aggr;
-                assignField(i + 1, false);
-              }
-            }
-          }
-
-          if (opt.genBin) {
-            // bin the field instead!
-            delete tf[i].aggr;
-            tf[i].bin = true;
-            tf[i].type = 'Q';
-            assignField(i + 1, hasAggr);
-          }
-
-          if (opt.genTypeCasting) {
-            // we can also change it to dimension (cast type="O")
-            delete tf[i].aggr;
-            delete tf[i].bin;
-            tf[i].type = 'O';
-            assignField(i + 1, hasAggr);
-          }
-        }
+      case 'T':
+        assignT(i, hasAggr);
         break;
 
       case 'O':
--- test/gen/aggregates.spec.js
+++ test/gen/aggregates.spec.js
@@ -1,37 +1,77 @@
 var expect = require('chai').expect,
   vl = require('vegalite');
 
-var genAggregates = require('../../src/gen/aggregates');
+var genProjections = require('../../src/gen/projections'),genAggregates = require('../../src/gen/aggregates'),
+  constsAggregates = require('../../src/consts').gen.aggregates;
+
 
 describe('vr.gen.aggregates()', function () {
   describe('1Q', function () {
     var fields = [{name:1, type:'Q'}];
+
+
     var tables = genAggregates([], fields);
 
-    it('should output 4 data table', function () {
-      // each field can be Q, avg(Q), bin(Q), O
-      expect(tables.length).to.equal(4);
+    it('should output 1 data table', function () {
+      expect(tables.length).to.equal(1);
     });
 
     it('should append key to each fieldSet', function() {
       expect(tables[0].key).to.be.ok();
     });
+
+    it('should output 4 data table if not omit', function () {
+      var tables = genAggregates([], fields, {
+        omitMeasureOnly: false,
+        omitDimensionOnly: false
+      });
+      console.log(tables);
+      // each field can be Q, avg(Q), bin(Q), O
+      expect(tables.length).to.equal(4);
+    });
   });
 
   describe('2Q with genTypeCasting', function () {
     var fields = [{name:1, type:'Q'}, {name:2, type:'Q'}];
+
+    // TODO add one more test here
+
     var tables = genAggregates([], fields, {
-      genAggr: true,
-      genTypeCasting: true
-    });
+        omitMeasureOnly: false,
+        omitDimensionOnly: false
+      });
 
     // except that two pairs of (avg(Q),Q) doesn't work
-    it('should output 14 data table', function () {
+    it('should output 14 data table if not omit', function () {
       expect(tables.length).to.equal(14);
     });
 
     it('should append key to each fieldSet', function() {
       expect(tables[12].key).to.be.ok();
+    });
+  });
+
+  describe('1Q & 1 count', function () {
+    var fields = [{name:1, type:'Q'}, vl.field.count()];
+    var tables = genAggregates([], fields);
+    it('should output 2 data table', function () {
+      expect(tables.length).to.equal(2); // O, bin
+    });
+  });
+
+  describe('1O & 1 count', function () {
+    var fields = [{name:1, type:'O'}, vl.field.count()];
+    var tables = genAggregates([], fields);
+    it('should output 1 data table', function () {
+      expect(tables.length).to.equal(1); // O
+    });
+  });
+
+  describe('1 count', function () {
+    var fields = [vl.field.count()];
+    var tables = genAggregates([], fields);
+    it('should output no data table', function () {
+      expect(tables.length).to.equal(0); // O, bin
     });
   });
 
@@ -46,4 +86,16 @@ describe('vr.gen.aggregates()', function () {
 
   });
 
+  describe('after vr.gen.projections()', function() {
+    var fields = [
+      {name:1, type:'Q', selected: true},
+      {name:2, type:'Q', selected: false},
+      {name:3, type:'O', selected: false},
+      {name:'*', aggr:'count', selected: false}
+    ];
+
+    var projections = genProjections(fields);
+    console.log(projections);
+  })
 });
+
