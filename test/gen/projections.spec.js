0ea6cd9d31787dd75a599daee7307e47ea125385
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-03-11 01:17:35 -0700

    add test for preserving order in gen projections

--- test/gen/projections.spec.js
+++ test/gen/projections.spec.js
@@ -14,14 +14,18 @@ describe('vr.gen.projections()', function () {
   });
 
   describe('with a set of fields without any field selected', function () {
-    var fields = [
-      {name:1, selected: true},
-      {name:2, selected: true},
-      {name:3, selected: undefined},
-      {name:4, selected: undefined}
-    ];
+    var f = fixture.birdstrikes;
 
-    var projections = genProjections(fields);
+    var projections = genProjections(f.fields, f.stats);
+
+    it('should preserve name order', function() {
+      for (var i=0; i<projections.length-1 ; i++) {
+        var a = projections[i][0], b=projections[i+1][0];
+        if (a.type === b.type) {
+          expect(a.name).to.lt(b.name);
+        }
+      }
+    })
   });
 
   describe('with a set of fields', function () {
