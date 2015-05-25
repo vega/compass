a8c639cde13ea359ec128cb32ad078c0377a4c31
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-05-25 14:37:51 -0700

    append build to publish

--- publish.sh
+++ publish.sh
@@ -7,25 +7,25 @@ NC='\033[0m' # No Color
 type jq >/dev/null 2>&1 || { echo >&2 "I require jq but it's not installed.  Aborting."; exit 1; }
 
 # 0.2 Check if all files are commited
-if [ -z "$(git status --porcelain)" ]; then 
+if [ -z "$(git status --porcelain)" ]; then
   echo "All tracked files are commited.  Publishing on npm and bower. \n"
-else 
+else
   echo "${RED}There are uncommitted files. Please commit or stash first!${NC} \n\n"
   git status
   exit 1
-fi 
-
-# 1. NPM PUBLISH 
-
-npm publish
-# exit if npm publish failed 
-rc=$?
-if [[ $rc != 0 ]]; then 
-	echo "${RED} npm publish failed.  Publishing cancelled. ${NC} \n\n"
-	exit $rc; 
 fi
 
-# 2. BOWER PUBLISH 
+# 1. NPM PUBLISH
+
+npm publish
+# exit if npm publish failed
+rc=$?
+if [[ $rc != 0 ]]; then
+	echo "${RED} npm publish failed.  Publishing cancelled. ${NC} \n\n"
+	exit $rc;
+fi
+
+# 2. BOWER PUBLISH
 
 # read version
 gitsha=$(git rev-parse HEAD)
@@ -43,5 +43,6 @@ git tag -am "Release v$version." "v$version"
 # now swap back to the clean master and push the new tag
 git checkout master
 git push --tags
+gulp build # rebuild -- so compiled files are back for linked bower
 
 # Woo hoo! Now the published tag contains compiled files which works great with bower.
